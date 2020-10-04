568902a780db17f339e2d3973555d7d8f22b4e25
Astacius <astacius94@gmail.com>
2020-10-04 02:09:18 +0200

    smishy i am retarded

---
+++ client/build/precache-manifest.8bb528b54a9a0152677753ffa0f46cee.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e027a3d3451896e3c2c3f3ad5c94d6d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4a388fe82767c4023a4",
+    "url": "/static/css/2.190caba9.chunk.css"
+  },
+  {
+    "revision": "8a9fe0de1dbe1f064cca",
+    "url": "/static/css/3.d06eeaec.chunk.css"
+  },
+  {
+    "revision": "16d4bb8aee9727a458df",
+    "url": "/static/css/main.faeaf4b4.chunk.css"
+  },
+  {
+    "revision": "a4a388fe82767c4023a4",
+    "url": "/static/js/2.71160f4a.chunk.js"
+  },
+  {
+    "revision": "0591dc3f8979d61906bef308dafca80a",
+    "url": "/static/js/2.71160f4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a9fe0de1dbe1f064cca",
+    "url": "/static/js/3.028bff18.chunk.js"
+  },
+  {
+    "revision": "16d4bb8aee9727a458df",
+    "url": "/static/js/main.75b9bef0.chunk.js"
+  },
+  {
+    "revision": "c54af95dbee2bf00b770",
+    "url": "/static/js/runtime-main.926d7f2b.js"
+  },
+  {
+    "revision": "feb48833bac136f91aab9b27583fb981",
+    "url": "/static/media/camera-stop.feb48833.svg"
+  },
+  {
+    "revision": "ff36203ce9be4c3b857c6e7648966b07",
+    "url": "/static/media/camera.ff36203c.svg"
+  },
+  {
+    "revision": "7722d32d9ff3e121881324b81968f255",
+    "url": "/static/media/fullscreen.7722d32d.svg"
+  },
+  {
+    "revision": "d3ae4842456ead735aecd20d34c2d80c",
+    "url": "/static/media/hang-up.d3ae4842.svg"
+  },
+  {
+    "revision": "bbec867b3bc32a35a52f068fe692f735",
+    "url": "/static/media/microphone-stop.bbec867b.svg"
+  },
+  {
+    "revision": "f375b0a7d2bf1c4703b4a98b5beeb1b8",
+    "url": "/static/media/microphone.f375b0a7.svg"
+  },
+  {
+    "revision": "612b180a63a0a715724db3a0be971688",
+    "url": "/static/media/minimize.612b180a.svg"
+  },
+  {
+    "revision": "0be3c651e1532de63056a20e2f341bde",
+    "url": "/static/media/ringtone.0be3c651.mp3"
+  },
+  {
+    "revision": "269beb7bd8d4931225aadb7103aa2be9",
+    "url": "/static/media/share.269beb7b.svg"
+  }
+]);
