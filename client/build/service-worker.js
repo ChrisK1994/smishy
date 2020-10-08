85f549260a1dcf9721ad0b71051bcc63f4728c0f
Chris K <krawczyk.krzysztof.1994@gmail.com>
2020-10-08 22:48:24 +0200

    cos

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94595f329e82777076dec73f1caa57a4.js"
+  "/precache-manifest.f8432bb49c40ab185fda8ac41c2119ff.js"
 );
 
 self.addEventListener('message', (event) => {
