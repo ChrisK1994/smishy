f8f08cf24cf9a80eeffa9cac665566493c4a490c
Chris K <krawczyk.krzysztof.1994@gmail.com>
2020-11-22 22:25:36 +0100

    build

---
+++ client/build/static/js/3.519be75a.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[3],{111:function(s,t,c){},112:function(s,t,c){"use strict";c.r(t);var e=c(0),a=(c(1),c(55));c(111);t.default=function(){return Object(e.jsx)("div",{className:"watermark",children:Object(e.jsx)("img",{className:"logoImage",src:a.a,alt:"Smishy Logo"})})}}}]);
+//# sourceMappingURL=3.519be75a.chunk.js.map
--- client/build/static/js/3.7659586b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[3],{109:function(s,t,c){},110:function(s,t,c){"use strict";c.r(t);var e=c(0),a=(c(1),c(55));c(109);t.default=function(){return Object(e.jsx)("div",{className:"watermark",children:Object(e.jsx)("img",{className:"logoImage",src:a.a,alt:"Smishy Logo"})})}}}]);
-//# sourceMappingURL=3.7659586b.chunk.js.map
