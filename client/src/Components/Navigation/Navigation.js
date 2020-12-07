a37badb5ad471745463ec5c6686b600acf554d4f
Chris K <krawczyk.krzysztof.1994@gmail.com>
2020-12-07 20:26:50 +0100

    better online now

--- client/src/Components/Navigation/Navigation.js
+++ client/src/Components/Navigation/Navigation.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "../Navigation/Navigation.scss";
+import { FaUser } from "react-icons/fa";
 
 const Navigation = (props) => {
   return (
     <header className="dropShadow">
-      <div className="headerWrapper">
-        <div className="headerContainer flex">
-          <div className="headerLogoLinkWrapper">
-              <div className="headerLogo flex flex-row">
-                <div className="logoText">Smishy</div>
-                <div className="onlineText"> - online now {props.online}</div>
-              </div>
+      <div className="headerContainer">
+        <div className="headerLogo">
+          <div className="logoText">Smishy</div>
+          <div className="onlineText">
+            {" "}
+            {props.online}{" "}
+            <FaUser className="onlineIcon" alt="users" /> online now
           </div>
         </div>
       </div>
